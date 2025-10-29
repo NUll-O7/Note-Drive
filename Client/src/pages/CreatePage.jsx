dc39e62400f21e96dade53a9640d7417ef640220
Dhruv Soni <dhruvsonimain@gmail.com>
2025-10-29 23:49:22 +0530

    Changes

--- Client/src/pages/CreatePage.jsx
+++ Client/src/pages/CreatePage.jsx
@@ -1,7 +1,5 @@
-import { ArrowLeftIcon } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { Link, useNavigate } from "react-router";
 import api from "../lib/axios";
 
 const CreatePage = () => {
